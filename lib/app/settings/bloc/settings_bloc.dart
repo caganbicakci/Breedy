0aa368edda817e33c7e5c229cdf182f84e03ff73
Çağan Bıçakçı <caganbicakci97@gmail.com>
2023-11-18 23:22:21 +0300

    settings page designed

--- lib/app/settings/bloc/settings_bloc.dart
+++ lib/app/settings/bloc/settings_bloc.dart
@@ -1,13 +1,47 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:breedy/app/settings/model/settings_model.dart';
+import 'package:breedy/l10n/l10n.dart';
 
 part 'settings_event.dart';
 part 'settings_state.dart';
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
-  SettingsBloc() : super(SettingsInitial()) {
+  SettingsBloc(this.l10n) : super(SettingsInitial()) {
+    final settingsList = [
+      SettingsModel(
+        title: l10n.help,
+        iconLink: 'assets/settings/help.svg',
+        isInfo: false,
+      ),
+      SettingsModel(
+        title: l10n.rate,
+        iconLink: 'assets/settings/rate.svg',
+        isInfo: false,
+      ),
+      SettingsModel(
+        title: l10n.share,
+        iconLink: 'assets/settings/share.svg',
+        isInfo: false,
+      ),
+      SettingsModel(
+        title: l10n.terms,
+        iconLink: 'assets/settings/terms.svg',
+        isInfo: false,
+      ),
+      SettingsModel(
+        title: l10n.privacy,
+        iconLink: 'assets/settings/privacy.svg',
+        isInfo: false,
+      ),
+      SettingsModel(
+        title: l10n.version,
+        iconLink: 'assets/settings/version.svg',
+        isInfo: true,
+      ),
+    ];
     on<SettingsEvent>((event, emit) {
-      // TODO: implement event handler
+      emit(SettingsReady(settingsList));
     });
   }
+  AppLocalizations l10n;
 }
--- lib/app/settings/view/settings_page.dart
+++ lib/app/settings/view/settings_page.dart
@@ -1,1 +1,57 @@
+import 'dart:io' show Platform;
 
+import 'package:breedy/app/settings/bloc/settings_bloc.dart';
+import 'package:breedy/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => SettingsBloc(context.l10n),
+      child: SettingsView(),
+    );
+  }
+}
+
+class SettingsView extends StatelessWidget {
+  SettingsView({super.key});
+  final osVersion = Platform.operatingSystemVersion;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SettingsBloc, SettingsState>(
+      builder: (context, state) {
+        if (state is SettingsInitial) {
+          context.read<SettingsBloc>().add(SettingsEvent());
+        }
+        if (state is SettingsReady) {
+          return ListView.separated(
+            separatorBuilder: (_, index) {
+              return const Divider(
+                color: Color(0xffE5E5EA),
+                thickness: 2,
+                height: 2,
+              );
+            },
+            itemCount: state.settingsList.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(state.settingsList[index].title),
+                leading: SvgPicture.asset(state.settingsList[index].iconLink),
+                trailing: state.settingsList[index].isInfo == true
+                    ? Text(osVersion)
+                    : SvgPicture.asset('assets/settings/arrow.svg'),
+              );
+            },
+          );
+        }
+        return Container();
+      },
+    );
+  }
+}
